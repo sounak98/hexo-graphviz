5c63afdd579de0108d4bfd04680ab0ce5ee4fa6b
Lpmvb <lpmvbs@gmail.com>
2019-01-30 14:03:13 +0800

    refactor

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const assign = require('deep-assign');
 
 const config = {
-    engine: 'dot',
+  engine: 'dot',
 };
 
-hexo.config.graphviz = assign({
-    enable: true
-}, config, hexo.config.graphviz);
+hexo.config.graphviz = assign(
+  {
+    enabled: true,
+  },
+  config,
+  hexo.config.graphviz,
+);
 
 hexo.extend.filter.register('before_post_render', require('./lib/render'), 9);
